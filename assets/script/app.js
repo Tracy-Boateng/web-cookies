18ddf667bb87eea0a2b37cfd02f3343db7077d43
Tracy <trac.boats.tb@gmail.com>
2025-11-28 23:37:42 -0600

    getCookie func

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,3 +7,18 @@ function setCookie(name, value, seconds = 20) {
   const expires = "expires=" + d.toUTCString();
   document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; ${expires}; path=/`;
 }
+
+function getCookie(name) {
+  const cookieStr = document.cookie;
+  const nameEQ = encodeURIComponent(name) + "=";
+  const parts = cookieStr.split("; ").map(c => c.trim());
+  for (const p of parts) {
+    if (p.indexOf(nameEQ) === 0) {
+      return decodeURIComponent(p.substring(nameEQ.length));
+    }
+  }
+  return "";
+}
+function anyCookieExists() {
+  return document.cookie && document.cookie.length > 0;
+}
